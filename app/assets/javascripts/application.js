184d9b9c4c1d68807842556ea5c829fe45192603
Connor Major <cm966337@yahoo.com>
2017-07-09 22:06:13 -0400

    part 6 7min 49 second

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,12 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("turbolinks:load", function() {
+  tinymce.remove();
+  tinymce.init({ 
+    selector:'textarea#post_body',
+    plugins: "codesample image media link code",
+    toolbar: "undo redo | styleselect | bold italic | codesample image media link code" 
+  });
+})
