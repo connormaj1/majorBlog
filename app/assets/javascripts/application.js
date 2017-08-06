0f984a3b32fee6b928cbba5284fd0df5cebc830d
Connor Major <cm966337@yahoo.com>
2017-08-06 19:48:11 -0400

    fix 1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,17 @@ document.addEventListener("turbolinks:load", function() {
     plugins: "codesample image media link code",
     toolbar: "undo redo | styleselect | bold italic | codesample image media link code"
   });
-})
+});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-103947732-1', 'auto');
+ga('send', 'pageview');
+
+(adsbygoogle = window.adsbygoogle || []).push({
+  google_ad_client: "ca-pub-6236162619240307",
+  enable_page_level_ads: true
+});
